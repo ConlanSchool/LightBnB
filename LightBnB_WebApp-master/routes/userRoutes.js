e4716a315ce1f38e5cf40ee59525cf350a4a50cb
ConlanSchool <conlan.school@gmail.com>
2023-04-19 17:47:09 -0700

    Add login and sign up functions

---
+++ LightBnB_WebApp-master/routes/userRoutes.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const database = require("../db/database");
+
+const router = express.Router();
+
+// Create a new user
+router.post("/", (req, res) => {
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 12);
+  database
+    .addUser(user)
+    .then((user) => {
+      if (!user) {
+        return res.send({ error: "error" });
+      }
+
+      req.session.userId = user.id;
+      res.send("🤗");
+    })
+    .catch((e) => res.send(e));
+});
+
+// Log a user in
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log("Attempting login with email:", email, "and password:", password);
+
+  database.getUserWithEmail(email).then((user) => {
+    if (!user) {
+      console.log("User not found in database for email:", email);
+      return res.send({ error: "no user with that id" });
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      console.log("Incorrect password for email:", email);
+      return res.send({ error: "error" });
+    }
+
+    req.session.userId = user.id;
+    res.send({
+      user: {
+        name: user.name,
+        email: user.email,
+        id: user.id,
+      },
+    });
+  });
+});
+
+
+// Log a user out
+router.post("/logout", (req, res) => {
+  req.session.userId = null;
+  res.send({});
+});
+
+// Return information about the current user (based on cookie value)
+router.get("/me", (req, res) => {
+  const userId = req.session.userId;
+  if (!userId) {
+    return res.send({ message: "not logged in" });
+  }
+
+  database
+    .getUserWithId(userId)
+    .then((user) => {
+      if (!user) {
+        return res.send({ error: "no user with that id" });
+      }
+
+      res.send({
+        user: {
+          name: user.name,
+          email: user.email,
+          id: userId,
+        },
+      });
+    })
+    .catch((e) => res.send(e));
+});
+
+module.exports = router;
